9ad088c11d61e1324b4eb5b7b3c1389a63554b23
idkuserlua <jd262716@gmail.com>
2024-12-26 23:40:00 -0500

    Update verab.lua

--- verab.lua
+++ verab.lua
@@ -1,2 +1,2 @@
 
-game:GetService("Players").LocalPlayer:Kick("Reason: Script is discontinued Please Use Blitz/Vhs/Wexort")
+game:GetService("Players").LocalPlayer:Kick("Reason: Script is discontinued Please Use Blitz/Vhs/Wexort im sorry i did this im struggling with my life im at my lowest i regret everything goodbye/ by jose/itsjose4")
