793a416c5364071c94a8aafd1aba04bc17f83009
idkuserlua <jd262716@gmail.com>
2024-12-26 23:09:41 -0500

    Update verab.lua

--- verab.lua
+++ verab.lua
@@ -1,2 +1,2 @@
 
-game:GetService("Players").LocalPlayer:Kick("Reason: Script is discontinued")
+game:GetService("Players").LocalPlayer:Kick("Reason: Script is discontinued Please Use Blitz/Vhs/Wexort")
